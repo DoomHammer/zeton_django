80a2485f88109dcd2ad6ef08e72c140edc7a3393
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-06-27 14:52:46 +0200

    create points model

--- zeton_backend/points/models.py
+++ zeton_backend/points/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Point(models.Model):
+    ORIGIN = (
+        ('home', 'home'),
+        ('school', 'school'),
+    )
+    origin = models.CharField(max_length=100, null=True, choices=ORIGIN)
+    value = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.origin
