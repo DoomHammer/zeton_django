359e1e23cff11eb8abc2766f4c09acd11934190f
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-06-29 09:10:50 +0200

    change model points value to integerfield

--- zeton_backend/points/models.py
+++ zeton_backend/points/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Point(models.Model):
     ORIGIN = (
         ('home', 'home'),
         ('school', 'school'),
     )
     origin = models.CharField(max_length=100, null=True, choices=ORIGIN)
-    value = models.PositiveIntegerField()
+    value = models.IntegerField()
 
     def __str__(self):
         return self.origin
